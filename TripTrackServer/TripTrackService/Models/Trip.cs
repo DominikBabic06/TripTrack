6e5b5c267d1ce1c0ed88137f9ee5c8220231b33c
Dominik Babic <bdominik06@yahoo.com>
2024-10-13 21:55:53 +0200

    Kleine Änderungen

---
+++ TripTrackServer/TripTrackService/Models/Trip.cs
@@ -0,0 +1,9 @@
+namespace TripTracker.Models
+{
+    public class Trip
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set }
+        public 
+    }  
+}
---
+++ TripTrackServer/TripTrackService/Models/User.cs
@@ -0,0 +1,11 @@
+namespace TripTracker.Models
+{
+    public struct User
+    {
+         int ID { get; set; }
+         string userName { get; set; }
+         string eMail { get; set; }
+         string realName { get; set; }
+    }
+
+}
